2561c2e5b386710027526b3edbd3ebd734774613
Prathamesh Karbele <karbeleprathamesh@gamil.com>
2026-01-30 20:12:18 +0530

    gradle sycn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,4 +92,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.compose.material:material-icons-extended:1.7.0")
+
+    //want to push
 }
